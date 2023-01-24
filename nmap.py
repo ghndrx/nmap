055825c076abe98f24b8242dd16c5b586b03b41c
Greg Hendrickson <Jebus47@ymail.com>
2023-01-24 06:06:02 -0800

    Update nmap.py
    I have aligned the menu options in a neat and organized manner, making it easier to read for the user.
    I added a colon after the "Choose from above" prompt to make it clear that it is a question.
    I changed the case of the selected message from all upper case to Title case for readability.

--- nmap.py
+++ nmap.py
@@ -10,35 +10,34 @@ def fpingTxt():
 
 def menu():
     print("""
-#############################
-#                           #
-#      CHOOSE ARGUMENTS     #
-#         FOR NMAP          #
-#                           #
-#############################
+########################################
+#                                      #
+#      CHOOSE ARGUMENTS FOR NMAP SCAN   #
+#                                      #
+########################################
 
-This is a list of precompiled slightly modular cmds
+This is a list of precompiled, slightly modular cmds
 By default all NMAP cmds are set to -T4
 -T4 sends packets at an aggressive rate or 1.25p/s
 
-#############################
+########################################
 0. HOST DISCOVERER - USES NMAP TO CHECK IF TARGET(s) ARE ALIVE
 1. FULL SCAN - ALL PORTS (0-65,535)
 2. TCP CONNECT, OS, DETAILED SERVICE SCAN FOR OPEN PORTS
 3. VIOLATE TCP CONNECTION WITH XMAS
 4. MASSCAN PORT 80 OF TARGET(s)
 Anything else to exit.
-#############################
+########################################
 """)
 
 def menuSelector():
-    menuChoice = input("Choose from above")
+    menuChoice = input("Choose from above: ")
     switch = {
-        "0": "0.Selected Host Discoverer...",
-        "1": "1.Selected Full Scan...",
-        "2": "2.Selected TCP Connect...",
-        "3": "3.Selected TCP Violation...",
-        "4": "4.Selected Masscan..."
+        "0": "0. Selected Host Discoverer...",
+        "1": "1. Selected Full Scan...",
+        "2": "2. Selected TCP Connect...",
+        "3": "3. Selected TCP Violation...",
+        "4": "4. Selected Masscan..."
     }
     print(switch.get(menuChoice, "Exiting..."))
     return menuChoice
